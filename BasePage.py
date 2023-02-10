e933a41effc84a3b65f033cf7da86b3fa1b01042
SirPelmesh <ketrinquli@gmail.com>
2023-02-10 13:38:43 +0300

    Files

---
+++ BasePage.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from BaseElement import BaseElement
+
+
+class BasePage(ABC):
+    #локатор
+    def __init__(self,locator):
+        self.Locator=locator
+
+    def is_opened(self):
+        if BaseElement(self.Locator).element_is_present():
+            return True
+
+    def is_closed(self):
+        if BaseElement(self.Locator).element_is_not_present():
+            return True
+
+'''   @abstractmethod
+    def return_locator(self):
+        return self.Locator'''
---
+++ browser_config.py
@@ -0,0 +1,2 @@
+class BrowserConfig(object):
+    BROWSER_NAME = "chrome"
